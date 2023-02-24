988609436d75b2ec3e4e8b0c5550f50e482b42e3
Renê Rodrigues <renerodriguessantana@gmail.com>
2023-02-23 22:16:18 -0300

    foram adicionadas as classes referentes a consulta

---
+++ api/src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -0,0 +1,16 @@
+package med.voll.api.domain.consulta;
+
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosAgendamentoConsulta(
+        Long idMedico,
+
+        @NotNull Long idPaciente,
+
+        @NotNull @Future LocalDateTime data
+       // ,        Especialidade especialidade
+        ) {
+}
